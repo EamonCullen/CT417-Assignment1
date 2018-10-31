48a6b95b3142611abc6b826aa38dd056efa45bc1
EamonCullen <eamonc123@gmail.com>
2018-10-31 12:30:47 +0000

    Added toString
    toString methods for all classes, minor fixes

--- src/mainPackage/Course.java
+++ src/mainPackage/Course.java
@@ -16,11 +16,29 @@ public class Course {
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
+	
+	public String toString(Course course) {
+		String toString = ("Course: ");
+		toString += course.courseName;
+		
+		toString = ("Modules: ");
+		toString += course.modules;
+		
+		toString = ("Students: ");
+		toString += course.students;
+
+		toString = ("Start Date: ");
+		toString += course.startDate;
+
+		toString = ("End Date: ");
+		toString += course.endDate;
+		
+		return toString;
+	}
 
 	public String getCourseName() {
 		return courseName;
 	}
-
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
@@ -28,7 +46,6 @@ public class Course {
 	public String[] getModules() {
 		return modules;
 	}
-
 	public void setModules(String[] modules) {
 		this.modules = modules;
 	}
@@ -36,7 +53,6 @@ public class Course {
 	public String[] getStudents() {
 		return students;
 	}
-
 	public void setStudents(String[] students) {
 		this.students = students;
 	}
@@ -44,15 +60,13 @@ public class Course {
 	public DateTime getStartDate() {
 		return startDate;
 	}
-
 	public void setStartDate(DateTime startDate) {
 		this.startDate = startDate;
 	}
-
+	
 	public DateTime getEndDate() {
 		return endDate;
 	}
-
 	public void setEndDate(DateTime endDate) {
 		this.endDate = endDate;
 	}
